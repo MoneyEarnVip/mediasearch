de3e678fc4ffe8d7b760c92c43857a6a79cf29cf
Kevin <76389377+kevinn264@users.noreply.github.com>
2021-07-24 17:28:52 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -50,13 +50,14 @@ async def answer(bot, query):
                                                   offset=offset)
 
     for file in files:
-        disallowed_characters = "._!@|"
+        disallowed_characters = "._!|"
         username = file.file_name
     for character in disallowed_characters:
         username = username.replace(character, " ")
+        username = username.replace(@, "")
         caption = f"<code>{username}</code> {sample_msg}"
     if caption is None:
-        caption = f"<code>{file.file_name}</code> {sample_msg}"
+        caption = f"<code>{username}</code> {sample_msg}"
     results.append(
         InlineQueryResultCachedDocument(
             title=file.file_name,
