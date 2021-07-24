4c4f70e10b1608ff89495c5b7082b3d89e935c3d
Kevin <76389377+kevinn264@users.noreply.github.com>
2021-07-24 13:59:59 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -50,7 +50,7 @@ async def answer(bot, query):
                                                   offset=offset)
 
     for file in files:
-        disallowed_characters = "._!"
+        disallowed_characters = "._!@|"
         username = file.file_name
     for character in disallowed_characters:
         username = username.replace(character, " ")
